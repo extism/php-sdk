d5811fba942a8b82d071d143e0a2110b30dc50b0
Benjamin Eckel <bhelx@simst.im>
2023-09-13 14:49:37 -0500

    ci: fix CI

--- example/index.php
+++ example/index.php
@@ -2,10 +2,9 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$wasm = file_get_contents("../../wasm/code.wasm");
+$wasm = file_get_contents("../wasm/count-vowels.wasm");
 $plugin = new \Extism\Plugin($wasm);
 
 $output = $plugin->call("count_vowels", "this is an example");
 $json = json_decode(pack('C*', ...$output));
 echo "Vowels counted = " . $json->{'count'} . PHP_EOL;
-
